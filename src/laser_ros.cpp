788350729db01ec4358994c62fa665dc5ba14978
cgd_Lab16 <cgdsss@mail.ustc.edu.cn>
2018-09-05 18:35:20 +0800

    laser buffer

--- src/laser_ros.cpp
+++ src/laser_ros.cpp
@@ -5,12 +5,39 @@ LaserRos::LaserRos()
     ros::NodeHandle private_nh("~");
     private_nh.param("a", a_, 2.0);
     private_nh.param("b", b_, 2.0);
+    laser_buffer_ = new RosLaserData[LaserBufferLen];
+    curLaserBufferIndex = -1;
 
     laser_subscriber_ = nh_.subscribe("/scan", 1, &LaserRos::laserCallback, this);
     markers_publisher_ = nh_.advertise<visualization_msgs::MarkerArray>("/scan_filter", 1);
 }
 
+LaserRos::~LaserRos()
+{
+    for (int i = 0; i < LaserBufferLen; i++)
+    {
+      if(laser_buffer_[i].data != NULL)
+        delete[] laser_buffer_[i].data;
+    }
+    delete[] laser_buffer_;
+    std::cout << "Free laser buffer" << std::endl;
+}
+
 void LaserRos::laserCallback(const sensor_msgs::LaserScanConstPtr laser_msg)
 {
+    laserBufferMutex.lock();
+    laser_num_ = laser_msg->ranges.size();
+    laser_angle_increment_ = laser_msg->angle_increment;
+    laser_angle_min_ = laser_msg->angle_min;
+    laser_angle_max_ = laser_msg->angle_max;
+    curLaserBufferIndex = (curLaserBufferIndex + 1) % LaserBufferLen;
+    ROS_INFO("%d", curLaserBufferIndex);
+    RosLaserData & laserData = laser_buffer_[curLaserBufferIndex];
+    if (laserData.data == NULL)
+      laserData.data = new double[laser_num_];
 
+    laserData.stamp = laser_msg->header.stamp;
+    for (int i = 0; i < laser_num_; i++)
+        laserData.data[i] = laser_msg->ranges[i];
+    laserBufferMutex.unlock();
 }
--- src/laser_ros.h
+++ src/laser_ros.h
@@ -1,15 +1,38 @@
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <boost/thread.hpp>
+
+class RosLaserData
+{
+public:
+  RosLaserData()
+  {
+    stamp = ros::Time(0);
+    data = NULL;
+  }
+  ros::Time stamp;
+  double* data;
+};
 
 class LaserRos
 {
 public:
     LaserRos();
+    ~LaserRos();
 private:
     ros::NodeHandle nh_;
     double a_;
     double b_;
+
+    boost::recursive_mutex laserBufferMutex;
+    RosLaserData *laser_buffer_;
+    int curLaserBufferIndex;
+    float laser_angle_min_;
+    float laser_angle_max_;
+    float laser_angle_increment_;
+    int laser_num_;
+    const static int LaserBufferLen = 20;
 
     ros::Subscriber laser_subscriber_;
     void laserCallback(const sensor_msgs::LaserScanConstPtr laser_msg);
