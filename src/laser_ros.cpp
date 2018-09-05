8b84496f5378a3aa77117f663d3817c79282deb0
cgd_Lab16 <cgdsss@mail.ustc.edu.cn>
2018-09-05 11:53:59 +0800

    establish rossub rospub rossrv rosparam

--- src/laser_ros.cpp
+++ src/laser_ros.cpp
@@ -1,1 +1,16 @@
 #include "laser_ros.h"
+
+LaserRos::LaserRos()
+{
+    ros::NodeHandle private_nh("~");
+    private_nh.param("a", a_, 2.0);
+    private_nh.param("b", b_, 2.0);
+
+    laser_subscriber_ = nh_.subscribe("/scan", 1, &LaserRos::laserCallback, this);
+    markers_publisher_ = nh_.advertise<visualization_msgs::MarkerArray>("/scan_filter", 1);
+}
+
+void LaserRos::laserCallback(const sensor_msgs::LaserScanConstPtr laser_msg)
+{
+
+}
--- src/laser_ros.h
+++ src/laser_ros.h
@@ -1,7 +1,20 @@
 #include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <visualization_msgs/MarkerArray.h>
 
 class LaserRos
 {
 public:
-    LaserRos() {}
+    LaserRos();
+private:
+    ros::NodeHandle nh_;
+    double a_;
+    double b_;
+
+    ros::Subscriber laser_subscriber_;
+    void laserCallback(const sensor_msgs::LaserScanConstPtr laser_msg);
+
+    ros::Publisher markers_publisher_;
+
+    ros::ServiceServer laser_service_;
 };
